e2c44aee739d2037fbbb07ebc9b1744ff9da59c3
Diogo Almazan <diogodpa@outlook.com>
2021-10-24 20:55:20 -0300

    Projeto app iniciado e configurações iniciais

---
+++ heat-app/src/components/Message/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+import { UserPhoto } from "../UserPhoto";
+
+import { styles } from "./styles";
+
+export function Message() {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.message}>Texto da mensagem</Text>
+
+			<View>
+				<UserPhoto imageUri="https://github.com/diogo-dpa.png" sizes="SMALL" />
+				<Text style={styles.userName}>Nome do usuário</Text>
+			</View>
+		</View>
+	);
+}
