e19d9b9527f2f60500e64266975ca5248d9becee
Diogo Almazan <diogodpa@outlook.com>
2021-10-24 21:50:21 -0300

    Components finalizados

--- heat-app/src/components/Message/index.tsx
+++ heat-app/src/components/Message/index.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 
 import { View, Text } from "react-native";
+import { MotiView } from "moti";
 import { UserPhoto } from "../UserPhoto";
 
 import { styles } from "./styles";
 
-export function Message() {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.message}>Texto da mensagem</Text>
+export type MessageProps = {
+	id: string;
+	text: string;
+	user: {
+		name: string;
+		avatar_url: string;
+	};
+};
 
-			<View>
-				<UserPhoto imageUri="https://github.com/diogo-dpa.png" sizes="SMALL" />
-				<Text style={styles.userName}>Nome do usuário</Text>
+type Props = {
+	data: MessageProps;
+};
+
+export function Message({ data }: Props) {
+	return (
+		<MotiView
+			from={{ opacity: 0, translateY: -50 }}
+			animate={{ opacity: 1, translateY: 0 }}
+			transition={{ type: "timing", duration: 700 }}
+			style={styles.container}
+		>
+			<Text style={styles.message}>{data.text}</Text>
+
+			<View style={styles.footer}>
+				<UserPhoto imageUri={data.user.avatar_url} sizes="SMALL" />
+				<Text style={styles.userName}>{data.user.name}</Text>
 			</View>
-		</View>
+		</MotiView>
 	);
 }
---
+++ heat-app/src/components/MessageList/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import { Message } from "../Message";
+import { styles } from "./styles";
+
+export function MessageList() {
+	const message = {
+		id: "1",
+		text: "mensagem de teste",
+		user: {
+			name: "Diogo",
+			avatar_url: "https://github.com/diogo-dpa.png",
+		},
+	};
+	return (
+		<ScrollView
+			style={styles.container}
+			contentContainerStyle={styles.content}
+			keyboardShouldPersistTaps="never"
+		>
+			<Message data={message} />
+			<Message data={message} />
+			<Message data={message} />
+		</ScrollView>
+	);
+}
